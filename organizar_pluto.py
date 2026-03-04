db990bdbe3814b1e5d75974b32d00ede071c344f
hermens16 <103794291+hermens16@users.noreply.github.com>
2026-03-04 19:11:37 -0300

    Atualizar o organizar_pluto.py

--- organizar_pluto.py
+++ organizar_pluto.py
@@ -1,24 +1,30 @@
-import requests
+import json
+import urllib.request
 
 saida = "pluto_br_final.m3u8"
 
-url = "https://api.pluto.tv/v2/channels?start=0&stop=999&country=BR"
+url = "https://api.pluto.tv/v2/channels?start=0&stop=500&country=BR"
 
-response = requests.get(url)
-canais = response.json()
+with urllib.request.urlopen(url) as response:
+    data = json.loads(response.read().decode())
 
 with open(saida, "w", encoding="utf-8") as f:
     f.write("#EXTM3U\n")
 
-    for canal in canais:
+    for canal in data:
         nome = canal["name"].upper()
+
+        if nome.startswith("PLUTO TV"):
+            nome = nome[8:].strip()
+
         grupo = canal.get("category", "PLUTO").upper()
         logo = canal.get("solidLogoPNG", {}).get("path", "")
+        canal_id = canal["_id"]
 
-        stream = f"https://service-stitcher.clusters.pluto.tv/stitch/hls/channel/{canal['_id']}/master.m3u8?deviceDNT=0"
+        stream = f"https://service-stitcher.clusters.pluto.tv/stitch/hls/channel/{canal_id}/master.m3u8?deviceDNT=0"
 
         f.write(
-            f'#EXTINF:-1 tvg-id="{canal["_id"]}" tvg-logo="{logo}" group-title="{grupo}",{nome}\n'
+            f'#EXTINF:-1 tvg-id="{canal_id}" tvg-logo="{logo}" group-title="{grupo}",{nome}\n'
         )
         f.write(stream + "\n")
 
