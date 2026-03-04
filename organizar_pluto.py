cd610d581a7fcf0ea29e68327adcbb2de58cb932
hermens16 <hermens16@hotmail.com>
2026-03-03 21:02:56 -0300

    Primeira versão Pluto BR

---
+++ organizar_pluto.py
@@ -0,0 +1,34 @@
+origem = r"C:\Users\User\Desktop\pluto_test\pluto_tv_scraper\plutotv_br.m3u8"
+saida = "pluto_br_final.m3u8"
+
+with open(saida, "w", encoding="utf-8") as out:
+    with open(origem, "r", encoding="utf-8") as f:
+        for linha in f:
+
+            if linha.startswith("#EXTINF"):
+
+                # Deixar grupo em maiúsculo
+                if 'group-title="' in linha:
+                    inicio = linha.find('group-title="') + 13
+                    fim = linha.find('"', inicio)
+                    grupo = linha[inicio:fim]
+                    linha = linha.replace(
+                        f'group-title="{grupo}"',
+                        f'group-title="{grupo.upper()}"'
+                    )
+
+                # Processar nome do canal
+                partes = linha.split(",")
+                nome = partes[1].strip()
+
+                # remover "Pluto TV " do começo
+                if nome.lower().startswith("pluto tv"):
+                    nome = nome[8:].strip()
+
+                nome = nome.upper()
+
+                linha = linha.split(",")[0] + ", " + nome + "\n"
+
+            out.write(linha)
+
+print("Pluto organizada com sucesso!")
