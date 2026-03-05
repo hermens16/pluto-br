df0c3ac1caaf4fb2a15daa26a217776ea0d9b693
hermens16 <hermens16@hotmail.com>
2026-03-05 17:22:14 -0300

    Atualiza├º├úo autom├ítica Pluto

--- atualizar_pluto.bat
+++ atualizar_pluto.bat
@@ -2,14 +2,12 @@ cd C:\Users\User\Desktop\pluto_test\pluto_tv_scraper
 
 node index.js
 
-copy plutotv_br.m3u8 C:\Users\User\Desktop\pluto_final\plutotv_br.m3u8
+copy plutotv_br.m3u8 C:\Users\User\Desktop\pluto_final\plutotv_br.m3u8 /Y
 
 cd C:\Users\User\Desktop\pluto_final
 
 py organizar_pluto.py
 
 git add .
-
 git commit -m "Atualização automática Pluto"
-
 git push origin main
