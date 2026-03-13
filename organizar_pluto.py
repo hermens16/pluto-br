3b320f2a07b2bd137eaf0b6a2dcbbb988be4f56f
hermens16 <hermens16@hotmail.com>
2026-03-13 15:41:58 -0300

    Atualiza├º├úo autom├ítica Pluto

--- organizar_pluto.py
+++ organizar_pluto.py
@@ -1,6 +1,25 @@
 origem = "plutotv_br.m3u8"
 saida = "pluto_br_final.m3u8"
 
+# grupos que irão virar SÉRIES
+series = [
+    "JORNADA NAS ESTRELAS",
+    "RETRÔ",
+    "NOVELAS",
+    "REALITY",
+    "MTV",
+    "ESTILO DE VIDA",
+    "SOUTH PARK"
+]
+
+# grupos que irão virar DOCUMENTÁRIOS
+documentarios = [
+    "NATUREZA",
+    "CURIOSIDADES",
+    "MISTÉRIOS E SOBRENATURAL",
+    "INVESTIGAÇÃO"
+]
+
 with open(origem, "r", encoding="utf-8") as f:
     linhas = f.readlines()
 
@@ -13,11 +32,27 @@ with open(saida, "w", encoding="utf-8") as out:
             if 'group-title="' in linha:
                 inicio = linha.find('group-title="') + 13
                 fim = linha.find('"', inicio)
-                grupo = linha[inicio:fim]
+                grupo = linha[inicio:fim].upper()
+
+                # regras de agrupamento
+                if grupo in series:
+                    grupo = "SÉRIES"
+
+                elif grupo in documentarios:
+                    grupo = "DOCUMENTÁRIOS"
+
+                elif grupo == "NICKELODEON":
+                    grupo = "INFANTIL"
+
+                elif grupo == "TV BRASILEIRA":
+                    grupo = "TV ABERTA"
+
+                elif grupo == "ANIME & GEEK":
+                    grupo = "ANIME & TOKUSATSU"
 
                 linha = linha.replace(
-                    f'group-title="{grupo}"',
-                    f'group-title="{grupo.upper()}"'
+                    f'group-title="{linha[inicio:fim]}"',
+                    f'group-title="{grupo}"'
                 )
 
             partes = linha.split(",")
@@ -25,6 +60,7 @@ with open(saida, "w", encoding="utf-8") as out:
             if len(partes) > 1:
                 nome = partes[1].strip()
 
+                # remove prefixo PLUTO TV
                 if nome.lower().startswith("pluto tv"):
                     nome = nome[8:].strip()
 
